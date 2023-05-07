e77066b2b131438ea0503b624b8ec2edf7db9fcc
Envercan Yeni <envercan4006@gmail.com>
2023-05-08 00:17:25 +0300

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -95,31 +95,31 @@
 		/*  AJAX CONTACT FORM
         /* ----------------------------------------------------------- */
 
-		$(".contactform").on("submit", function() {
-			$(".output_message").text("Gönderiliyor...");
-
-			var form = $(this);
-			$.ajax({
-				url: form.attr("action"),
-				method: form.attr("method"),
-				data: form.serialize(),
-				success: function(result) {
-					if (result == "success") {
-						$(".form-inputs").css("display", "none");
-						$(".box p").css("display", "none");
-						$(".contactform").find(".output_message").addClass("success");
-						$(".output_message").text("Başarıyla Gönderildi");
-					} else {
-						$(".tabs-container").css("height", "440px");
-
-						$(".contactform").find(".output_message").addClass("error");
-						$(".output_message").text("Gönderilirken Hata Oluştu!");
-					}
-				}
-			});
-
-			return false;
-		});
+//		$(".contactform").on("submit", function() {
+//			$(".output_message").text("Gönderiliyor...");
+//
+//			var form = $(this);
+//			$.ajax({
+//				url: form.attr("action"),
+//				method: form.attr("method"),
+//				data: form.serialize(),
+//				success: function(result) {
+//					if (result == "success") {
+//						$(".form-inputs").css("display", "none");
+//						$(".box p").css("display", "none");
+//						$(".contactform").find(".output_message").addClass("success");
+//						$(".output_message").text("Başarıyla Gönderildi");
+//					} else {
+//						$(".tabs-container").css("height", "440px");
+//
+//						$(".contactform").find(".output_message").addClass("error");
+//						$(".output_message").text("Gönderilirken Hata Oluştu!");
+//					}
+//				}
+//			});
+//
+//			return false;
+//		});
 
 	});
 
