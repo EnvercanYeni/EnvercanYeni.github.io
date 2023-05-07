b55110423da6bb54b3df3935f2c3d0fedc16c35c
Envercan Yeni <envercan4006@gmail.com>
2023-05-07 19:57:21 +0300

    Update send-email.php

--- send-email.php
+++ send-email.php
@@ -1,22 +1,32 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $surname = $_POST["surname"];
-    $email = $_POST["email"];
-    $subject = $_POST["subject"];
-    $message = $_POST["message"];
-    $cinsiyet = $_POST["cinsiyet"];
 
-    $to = "m.yeni4040@gmail.com";
-    $subject = "Yeni Bir Mesajınız Var: $subject";
-    $body = "Ad: $name\nSoyad: $surname\nE-posta: $email\nCinsiyet: $cinsiyet\n\nMesaj:\n$message";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Get form data
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $gender = $_POST['cinsiyet'];
+    $message = $_POST['message'];
 
-    $headers = "From: $email";
+    // Construct email message
+    $body = "Name: $name\nSurname: $surname\nEmail: $email\nGender: $gender\nSubject: $subject\n\nMessage:\n$message";
 
-    if (mail($to, $subject, $body, $headers)) {
-        echo "success";
+    // Set email headers
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-type: text/plain; charset=UTF-8\r\n";
+
+    // Send email
+    $to = 'envercan4006@gmail.com'; // Change this to your email address
+    $subject = 'New message from contact form';
+    $sent = mail($to, $subject, $body, $headers);
+
+    // Output success or error message
+    if ($sent) {
+        echo 'success';
     } else {
-        echo "error";
+        echo 'error';
     }
 }
 ?>
