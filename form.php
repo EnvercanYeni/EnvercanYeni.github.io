6bf75ff20204d801ed0cbffe93102ab55f236440
Envercan Yeni <envercan4006@gmail.com>
2023-05-07 23:14:49 +0300

    Update form.php

--- form.php
+++ form.php
@@ -1,54 +1,38 @@
 <?php
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+// Veritabanı bilgilerini ayarla
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "portfolyo";
 
-require 'phpmailer/src/Exception.php';
-require 'phpmailer/src/PHPMailer.php';
-require 'phpmailer/src/SMTP.php';
+// Veritabanına bağlan
+$conn = mysqli_connect($host, $username, $password, $database);
 
-require 'vendor/autoload.php';
-
-// Check if the form was submitted
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Get the form data
-    $name = $_POST['name'];
-    $surname = $_POST['surname'];
-    $email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
-    $cinsiyet = $_POST['cinsiyet'];
-
-    // Create a new PHPMailer instance
-    $mail = new PHPMailer(true);
-
-    try {
-        // Configure the SMTP settings
-        $mail->isSMTP();
-        $mail->Host = 'smtp.gmail.com';
-        $mail->SMTPAuth = true;
-        $mail->Username = 'yenitv4006@gmail.com';
-        $mail->Password = 'Envercan4006';
-        $mail->SMTPSecure = 'tls';
-        $mail->Port = 587;
-
-        // Set the sender and recipient email addresses
-        $mail->setFrom($email, $name . ' ' . $surname);
-        $mail->addAddress('envercan4006@gmail.com');
-
-        // Set the email subject and body
-        $mail->Subject = $subject;
-        $mail->Body = "Ad: $name\nSoyad: $surname\nCinsiyet: $cinsiyet\n\n$message";
-
-        // Send the email
-        $mail->send();
-
-        // Output a success message
-        echo 'Mesajınız başarıyla gönderildi. Teşekkürler!';
-
-    } catch (Exception $e) {
-        // Output an error message
-        echo 'Mesaj gönderilirken bir hata oluştu: ' . $mail->ErrorInfo;
-    }
+// Hata kontrolü yap
+if (!$conn) {
+    die("Veritabanı bağlantı hatası: " . mysqli_connect_error());
 }
+
+// Form verilerini al
+$name = $_POST['name'];
+$surname = $_POST['surname'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$cinsiyet = $_POST['cinsiyet'];
+$message = $_POST['message'];
+
+// SQL sorgusu oluştur
+$sql = "INSERT INTO mesaj (ad, soyad, email, konu, cinsiyet, mesaj) VALUES ('$name', '$surname', '$email', '$subject', '$cinsiyet', '$message')";
+
+// Sorguyu çalıştır ve sonucu kontrol et
+if (mysqli_query($conn, $sql)) {
+    echo "Mesajınız başarıyla gönderildi!";
+} else {
+    echo "Hata: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+// Veritabanı bağlantısını kapat
+mysqli_close($conn);
+
 ?>
